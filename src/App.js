d39f3e7a99a7745dbaa62f5dc55de1d81a7957f6
aboongm <aboongm@yahoo.com>
2022-05-05 14:00:53 +0530

    Install react-router-dom and react-icons, and add layout and components - books and categories

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './Layout';
+import Books from './components/Books';
+import Categories from './components/Categories';
+import NoPage from './components/NoPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import './index.css';
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Books />} />
+        <Route path="categories" element={<Categories />} />
+        <Route path="*" element={<NoPage />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
---
+++ src/components/Books.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Books = () => <div>Books</div>;
+
+export default Books;
